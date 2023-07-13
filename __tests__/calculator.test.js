af66a8c1ee9a4698e2055b486ecc56b0f7548af6
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-13 15:09:46 -0700

    refactor code and testing, add new test

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -2,17 +2,21 @@ import { GalacticAge } from "../src/calculator";
 
 describe('GalacticAge', () => {
   
-  test('should correctly create a GalacticAge object with five properties', () => {
-    const galacticAge = new GalacticAge(0, 0, 0, 0, 0);
-    expect(galacticAge.earthYears).toEqual(0);
-    expect(galacticAge.mercuryYears).toEqual(0);
-    expect(galacticAge.venusYears).toEqual(0);
-    expect(galacticAge.marsYears).toEqual(0);
-    expect(galacticAge.jupiterYears).toEqual(0);
+  let galacticAge;
+
+  beforeEach(() => {
+    galacticAge = new GalacticAge(10);
+  });
+
+  test('should correctly create a GalacticAge object with one property', () => {
+    expect(galacticAge.earthYears).toEqual(10);
   });
 
   test('should correctly calculate Mercury age and return it', () => {
-    const galacticAge = new GalacticAge(10);
-    expect(galacticAge.calculateMercuryAge()).toEqual(2.4)
+    expect(galacticAge.calculateMercuryAge()).toEqual(2.4);
+  });
+
+  test('should correctly calculate Venus age and return it', () => {
+    expect(galacticAge.calculateVenusAge()).toEqual(6.2);
   });
 })
--- src/calculator.js
+++ src/calculator.js
@@ -1,14 +1,14 @@
 export class GalacticAge {
-  constructor(earthYears, mercuryYears, venusYears, marsYears, jupiterYears) {
+  constructor(earthYears) {
     this.earthYears = earthYears;
-    this.mercuryYears = mercuryYears;
-    this.venusYears = venusYears;
-    this.marsYears = marsYears;
-    this.jupiterYears = jupiterYears;
   } 
 
   calculateMercuryAge() {
     this.mercuryYears = this.earthYears * .24;
     return this.mercuryYears;
+  }
+
+  calculateVenusAge() {
+    
   }
 }
