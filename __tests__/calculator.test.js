960adf47193db4f9e2165d82a71eb0c8649ace17
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-17 15:27:23 -0700

    refactor code and testing

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -25,11 +25,11 @@ describe('GalacticAge', () => {
 
   test('should return an object containing years passed since a previous birthday for all planets', () => {
     galacticAge = new GalacticAge(56);
-    expect(galacticAge.yearsPassed(43).Earth).toEqual(13);
-    expect(galacticAge.yearsPassed(43).Mercury).toEqual('54.17');
-    expect(galacticAge.yearsPassed(43).Venus).toEqual('20.97');
-    expect(galacticAge.yearsPassed(43).Mars).toEqual('6.91');
-    expect(galacticAge.yearsPassed(43).Jupiter).toEqual('1.10');
+    expect(galacticAge.yearsPassed(43).earth).toEqual(13);
+    expect(galacticAge.yearsPassed(43).mercury).toEqual('54.17');
+    expect(galacticAge.yearsPassed(43).venus).toEqual('20.97');
+    expect(galacticAge.yearsPassed(43).mars).toEqual('6.91');
+    expect(galacticAge.yearsPassed(43).jupiter).toEqual('1.10');
   });
 
   test('returns an error message if argument is greater than Earth age', () => {
@@ -39,11 +39,11 @@ describe('GalacticAge', () => {
 
   test('should return an object containing years until a future birthday for all planets', () => {
     galacticAge = new GalacticAge(56);
-    expect(galacticAge.yearsYetToPass(61).Earth).toEqual(5);
-    expect(galacticAge.yearsYetToPass(61).Mercury).toEqual('20.83');
-    expect(galacticAge.yearsYetToPass(61).Venus).toEqual('8.06');
-    expect(galacticAge.yearsYetToPass(61).Mars).toEqual('2.66');
-    expect(galacticAge.yearsYetToPass(61).Jupiter).toEqual('0.42');
+    expect(galacticAge.yearsYetToPass(61).earth).toEqual(5);
+    expect(galacticAge.yearsYetToPass(61).mercury).toEqual('20.83');
+    expect(galacticAge.yearsYetToPass(61).venus).toEqual('8.06');
+    expect(galacticAge.yearsYetToPass(61).mars).toEqual('2.66');
+    expect(galacticAge.yearsYetToPass(61).jupiter).toEqual('0.42');
   });
 
   test('returns an error message if argument is less than Earth age', () => {
--- src/calculator.js
+++ src/calculator.js
@@ -19,11 +19,11 @@ export class GalacticAge {
     }
     else {
       this.galacticYearsPassed = {};
-      this.galacticYearsPassed.Earth = this.earthYears - priorBirthday;
-      this.galacticYearsPassed.Mercury = (this.galacticYearsPassed.Earth / .24).toFixed(2);
-      this.galacticYearsPassed.Venus = (this.galacticYearsPassed.Earth / .62).toFixed(2);
-      this.galacticYearsPassed.Mars = (this.galacticYearsPassed.Earth / 1.88).toFixed(2);
-      this.galacticYearsPassed.Jupiter = (this.galacticYearsPassed.Earth / 11.86).toFixed(2);
+      this.galacticYearsPassed.earth = this.earthYears - priorBirthday;
+      this.galacticYearsPassed.mercury = (this.galacticYearsPassed.earth / .24).toFixed(2);
+      this.galacticYearsPassed.venus = (this.galacticYearsPassed.earth / .62).toFixed(2);
+      this.galacticYearsPassed.mars = (this.galacticYearsPassed.earth / 1.88).toFixed(2);
+      this.galacticYearsPassed.jupiter = (this.galacticYearsPassed.earth / 11.86).toFixed(2);
       return this.galacticYearsPassed;
     }
   }
@@ -35,11 +35,11 @@ export class GalacticAge {
     }
     else {
       this.galacticYearsYetToPass = {};
-      this.galacticYearsYetToPass.Earth = futureBirthday - this.earthYears;
-      this.galacticYearsYetToPass.Mercury = (this.galacticYearsYetToPass.Earth / .24).toFixed(2);
-      this.galacticYearsYetToPass.Venus = (this.galacticYearsYetToPass.Earth / .62).toFixed(2);
-      this.galacticYearsYetToPass.Mars = (this.galacticYearsYetToPass.Earth / 1.88).toFixed(2);
-      this.galacticYearsYetToPass.Jupiter = (this.galacticYearsYetToPass.Earth / 11.86).toFixed(2);
+      this.galacticYearsYetToPass.earth = futureBirthday - this.earthYears;
+      this.galacticYearsYetToPass.mercury = (this.galacticYearsYetToPass.earth / .24).toFixed(2);
+      this.galacticYearsYetToPass.venus = (this.galacticYearsYetToPass.earth / .62).toFixed(2);
+      this.galacticYearsYetToPass.mars = (this.galacticYearsYetToPass.earth / 1.88).toFixed(2);
+      this.galacticYearsYetToPass.jupiter = (this.galacticYearsYetToPass.earth / 11.86).toFixed(2);
       return this.galacticYearsYetToPass;
     }
   }
